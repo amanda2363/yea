61c0c736dfa4eb4b4afaf87821adcd7b7d5ecbc3
amanda2363 <125404520+amanda2363@users.noreply.github.com>
2023-05-26 15:40:03 -0300

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,20 @@
+  fun main() {
+    println("Olá Mundo")
+    var nome = "Amanda"
+    val nascimento = 2006
+
+    println(nome)   // exibe o nome
+    println(nascimento)  //exibe o nascimento
+
+  val myDoubleNum = 5.99    // Double
+  val myLetter = 'D'        // Char
+  val myBoolean = false      // Boolean
+  val myText = "Olá"      // String
+
+  println (myNum)
+  println (myDoubleNum)
+  println (myLetter)
+  println (myBoolean)
+  println (myText)
+  
+}
