d85ad8b14a4c1eb77dcdcfaff3708cdc94b0cf80
amanda2363 <125404520+amanda2363@users.noreply.github.com>
2023-05-29 15:35:44 -0300

    Add files via upload

---
+++ main (1).kt
@@ -0,0 +1,25 @@
+fun main() {
+  var x = 13
+  var y = 3
+  var soma = x + y
+  println("A Soma é:" +soma)
+  
+  var sub = x - y
+  println("A subtraçao é:" +sub)
+
+  var mult = x * y
+  println("A multiplicaçao é:" + mult)
+
+  var div = x / y
+  println("A divisao é: " + div)
+
+  var rest = x % y
+  println("O resto da divisao é: " + rest)
+
+  println ("O x é: " + x)
+  ++x
+  print("Agora o x é: " + x)
+  --x
+  println("agora o x menos 1: " + x)
+
+}
