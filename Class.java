e0685272b78345aa70d62593d9f31a1a0bb5b796
amanda2363 <125404520+amanda2363@users.noreply.github.com>
2023-05-31 16:02:45 -0300

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,10 @@
+class Main {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main Obj1 = new Main();
+    Main Obj2 = new Main();
+    System.out.println(Obj1.x);
+    System.out.println(Obj2.x);
+  }
+}
