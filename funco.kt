e76fc0f90a6b9063485826223b71efe99cc116c4
amanda2363 <125404520+amanda2363@users.noreply.github.com>
2023-06-06 17:44:53 -0300

    Add files via upload

---
+++ funco.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+  var nomeVariavel: Boolean = false
+  println(nomeVariavel)
+  var x: Boolean = true
+  println(x)
+
+  if (x) {
+    println("verdadeiro")
+  } else {
+    println("falso")
+  }
+}
